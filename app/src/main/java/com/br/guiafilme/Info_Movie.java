76bc3132f3ebe539a5c74da87060aeb52c831474
Michael Araújo <michaeldouglasl.araujo@gmail.com>
2018-11-29 00:16:06 -0200

    Alteração testes

---
+++ app/src/main/java/com/br/guiafilme/Info_Movie.java
@@ -0,0 +1,13 @@
+package com.br.guiafilme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Info_Movie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info__movie);
+    }
+}
---
+++ app/src/main/java/com/br/guiafilme/Movie_by_Genre.java
@@ -0,0 +1,13 @@
+package com.br.guiafilme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Movie_by_Genre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_by__genre);
+    }
+}
